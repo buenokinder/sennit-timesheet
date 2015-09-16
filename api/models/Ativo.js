a794f8ddd0d912fe6bc8f7785583d6dada2f7fba
Vinicius Baptista <vbaptista7@gmail.com>
2015-09-16 11:47:00 -0300

    Correção Ativo
    Nome da variavel alterada!

--- api/models/Ativo.js
+++ api/models/Ativo.js
@@ -13,7 +13,7 @@ module.exports = {
             type: 'string',
             required: true
         },
-       serailNumber: {
+       serialNumber: {
             type: 'string',
             required: true
         },
